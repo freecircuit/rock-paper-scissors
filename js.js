1c9b7cac92cbb520dcd3735a1c3cebe9652aecb3
freecircuit <178741088+freecircuit@users.noreply.github.com>
2024-09-14 00:05:43 -0700

    pseudocode for human choice

--- js.js
+++ js.js
@@ -6,4 +6,8 @@ console.log("hello")
  //assign resulting 3 (0, 1, 2) individual numbers to rock, paper, or scissors
 
 
- //
+ //gethumanchoice: prompt user for rock, paper, or scissors
+ //loop to repeat prompt if user enters something other than rock paper or scissors
+ //user input is reduced to same case (lower case) as computer
+ //assign choice = input
+ let choice = prompt("rock, paper or scissors?")
