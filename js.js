22bbbb64ee6632c42ea08d1fe2b449567f40bcb2
freecircuit <178741088+freecircuit@users.noreply.github.com>
2024-09-14 00:52:51 -0700

    code getComputerChoice()

--- js.js
+++ js.js
@@ -5,9 +5,25 @@ console.log("hello")
  //and % 3
  //assign resulting 3 (0, 1, 2) individual numbers to rock, paper, or scissors
 
-
+ function getComputerChoice()
+ {
+    const num = Math.floor(Math.random() * 10) % 3;
+    console.log(num)
+    if (num == 0) {
+        choice = "rock"
+    }
+    else if (num == 1) {
+        choice = "paper"
+    }
+    else {
+        choice = "scissors"
+    }
+    return choice
+    
+ }
+ console.log(getComputerChoice())
  //gethumanchoice: prompt user for rock, paper, or scissors
  //loop to repeat prompt if user enters something other than rock paper or scissors
  //user input is reduced to same case (lower case) as computer
  //assign choice = input
- let choice = prompt("rock, paper or scissors?")
+//  let choice = prompt("rock, paper or scissors?")
