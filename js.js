57a46ef86b493d252304b2ad9bf1ad2e95f2d746
freecircuit <178741088+freecircuit@users.noreply.github.com>
2024-09-13 23:41:22 -0700

    add pseudocode for computer choice

--- js.js
+++ js.js
@@ -1,1 +1,9 @@
 console.log("hello")
+
+ ///getcomputerchoice: increase math.random by factor of ten
+ //average down to nearest whole number
+ //and % 3
+ //assign resulting 3 (0, 1, 2) individual numbers to rock, paper, or scissors
+
+
+ //
