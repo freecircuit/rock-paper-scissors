76a2e07f05b6515b09942e71fd5ae06f68a3c23c
freecircuit <178741088+freecircuit@users.noreply.github.com>
2024-09-15 00:55:23 -0700

    create global variables from functions

--- js.js
+++ js.js
@@ -1,8 +1,5 @@
 console.log("hello")
 
-// Declare variables
-let comp = '';
-let human = '';
 
  ///getcomputerchoice: increase math.random by factor of ten
  //average down to nearest whole number
@@ -12,7 +9,7 @@ let human = '';
  function getComputerChoice()
  {
     const num = Math.floor(Math.random() * 10 + 1) % 3;
-    console.log(num)
+    // console.log(num)
     if (num == 0) {
        var comp = "rock"
     }
@@ -25,7 +22,6 @@ let human = '';
     return comp
     
  }
- console.log(getComputerChoice())
  //gethumanchoice: prompt user for rock, paper, or scissors
  //loop to repeat prompt if user enters something other than rock paper or scissors
  //user input is reduced to same case (lower case) as computer
@@ -36,14 +32,18 @@ let human = '';
     while (human.toLowerCase() !== 'rock' &&
             human.toLowerCase() !== 'paper' &&
             human.toLowerCase() !== 'scissors') {
-        human = prompt("rock, paper, scissor:")
+        human = prompt("Please choose either rock, paper, or scissors:")
     }
   return human
   
  
  }
  // assign values from functions to global constants
- const humChoice = getHumanChoice()
- const compChoice = getComputerChoice()
- console.log(alert( humChoice == 'rock'))
+ const humChoice = getHumanChoice().toLowerCase()
+ const compChoice = getComputerChoice().toLowerCase()
+ console.log(humChoice)
+ console.log(compChoice)
  
+humanScore = 0
+computerScore = 0
+
