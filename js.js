a94d4436cd3cd0a6697538079993b84f74ac4a3d
freecircuit <178741088+freecircuit@users.noreply.github.com>
2024-09-15 22:52:37 -0700

    code final game

--- js.js
+++ js.js
@@ -5,6 +5,7 @@ console.log("hello")
  //average down to nearest whole number
  //and % 3
  //assign resulting 3 (0, 1, 2) individual numbers to rock, paper, or scissors
+ 
 
  function getComputerChoice()
  {
@@ -39,10 +40,10 @@ console.log("hello")
  
  }
  // assign values from functions to global constants
- const humChoice = getHumanChoice().toLowerCase()
- const compChoice = getComputerChoice().toLowerCase()
- console.log(humChoice)
- console.log(compChoice)
+ var humChoice = getHumanChoice().toLowerCase()
+ var compChoice = getComputerChoice().toLowerCase()
+//  console.log(humChoice)
+//  console.log(compChoice)
 
  
     var humanScore = 0
@@ -51,59 +52,93 @@ console.log("hello")
 // create function for single round of game
 //take inputs from computer and user to utilize as variables of the function
 //set relationdship between rock, paper and scissors according to rules of the game
-//use switch statement for variable comparison, resulting increment of score, and text declaring winner/loser
+//use if-else statement for variable comparison, resulting increment of score, and text declaring winner/loser
 //
+var y = humanScore
+var x = computerScore
+
+
 function playRound(humanChoice, computerChoice){
-
-
-
+    
      if (humanChoice == "rock" && 
         computerChoice == "scissors"){
-            humanScore += 1
+            humanScore ++
             alert("You win! Rock bests Scissors");
      }
      else if (humanChoice == "rock" &&
             computerChoice == "paper"){
-                computerScore += 1
+                computerScore ++
                 alert("You Lose! Paper beats Rock")
             }
     else if(humanChoice == "paper" &&   
             computerChoice == "rock"){
-                humanScore += 1
+                humanScore ++
                 alert("You Win! Paper beats Rock!")
             }
     else if(humanChoice == "paper" &&
             computerChoice == "scissors"){
-                computerScore += 1
+                computerScore ++
                 alert("You Lose! Scissors beats Rock!")
             }
     else if(humanChoice == "scissors" &&
             computerChoice == "rock"){
-                computerScore += 1
+                computerScore ++
                 alert("You Lose! Rock beats Scissors!")
             }
     else if(humanChoice == "scissors" &&
             computerChoice == "paper"){
-            humanScore += 1
+            humanScore ++
             alert("You Win! Scissors beats Paper!")
             }
     else{
         alert("It's a tie!")
     }
 
-   
-    // switch (humChoice) {
-    //     case "rock" 
-    //         humanScore + 1;
-    //          
-    //         break;
-        
-    
-
-    //     default:
-    //         break;
+    // if(humanScore > x){
+    //     return humanScore
     // }
+    // else if(computerScore > y){
+    //     return computerScore
+    // }
+    // else{
+    //     return 0;
+    // }
+
 }
 // console.log(playRound(humChoice, compChoice))
-console.log(humanScore)
-console.log(computerScore)
+
+// console.log(humanScore)
+// console.log(computerScore)
+// const n = playRound(humChoice, compChoice)
+console.log(playGame(humanScore, computerScore))
+
+
+function playGame(x, y){
+    x = humanScore 
+    y = computerScore 
+    var ch = getComputerChoice()
+    var cho = getHumanChoice()
+
+    while(humanScore < 5 && computerScore < 5){
+        playRound(cho, ch)
+        console.log(`your score `, humanScore, `vs computer `, computerScore)
+        playGame(x, y)
+    }
+    while(humanScore >= 5 || computerScore >= 5){
+    if(humanScore == 5){
+        console.log("you win!!")
+        break
+    }
+    else if(computerScore == 5){
+        console.log("you lose!!")
+        break
+    }}
+        
+
+
+        
+
+}
+
+
+
